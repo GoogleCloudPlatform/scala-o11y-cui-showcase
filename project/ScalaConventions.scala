bc24991b423bf82e6a5a86b5c7636af5601b12de
Josh Suereth <joshuasuereth@google.com>
2023-05-15 11:29:44 -0400

    Add license to all files.

--- project/ScalaConventions.scala
+++ project/ScalaConventions.scala
@@ -6,6 +6,9 @@ object ScalaConventions extends AutoPlugin {
   override def projectSettings: Seq[Def.Setting[_]] = Seq(
     scalaVersion := Dependencies.scala3Version,
     organization := "com.google.example",
-    version := "0.1.0-SNAPSHOT"
+    version := "0.1.0-SNAPSHOT",
+    organizationName := "Google",
+    startYear := Some(2023),
+    licenses += ("Apache-2.0", new URL("https://www.apache.org/licenses/LICENSE-2.0.txt"))
   )
 }
